f5ed47f2b57536f8911d3bac69fb8fab5b7e3052
Michał Dziedziński <michal.marcin.dziedzinski@gmail.com>
2019-01-23 17:23:30 +0100

    Finished app

--- src/components/Claim.vue
+++ src/components/Claim.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="claimWrapper">
         <h1 class="claim">
-			SPACER
-		</h1>
+      SPACER
+    </h1>
         <p class="subclaim">
-			Begin your journey throught our amazing galaxy, and discover places you never evan heard of.
-		</p>
-    	<p class="subclaim">
-			Type anything space-related to start.
-		</p>
+      Begin your journey throught our amazing galaxy, and discover places you never evan heard of.
+    </p>
+      <p class="subclaim">
+      Type anything space-related to start.
+    </p>
     </div>
 </template>
 
@@ -26,15 +26,32 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    color: white;
   }
 
   .claim {
     font-size: 50px;
     margin: 0;
+
+    @media (min-width: 768px){
+      font-size: 80px;
+    }
+
+    @media (min-width: 1024px){
+      font-size: 100px;
+    }
   }
 
   .subclaim {
-    margin-top: 20px;
+    margin: 20px 0 0 0;
     font-size: 13px;
+    font-weight: 400;
+
+    @media (min-width: 768px){
+      width: 500px;
+      font-size: 17px;
+      line-height: 1;
+      letter-spacing: 1px;
+    }
   }
 </style>
